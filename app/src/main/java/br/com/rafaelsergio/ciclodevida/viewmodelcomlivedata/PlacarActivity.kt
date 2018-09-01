176747e6b6941fa6e90c5250be8cbdec984d7d3e
Rafael <logonrmlocal@fiap.com.br>
2018-09-01 17:17:37 -0300

    utilizando liveCycle e funcao reativa

--- app/src/main/java/br/com/rafaelsergio/ciclodevida/viewmodelcomlivedata/PlacarActivity.kt
+++ app/src/main/java/br/com/rafaelsergio/ciclodevida/viewmodelcomlivedata/PlacarActivity.kt
@@ -1,9 +1,11 @@
 package br.com.rafaelsergio.ciclodevida.viewmodelcomlivedata
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.rafaelsergio.ciclodevida.R
+import kotlinx.android.synthetic.main.activity_placar.*
 
 class PlacarActivity : AppCompatActivity() {
 
@@ -14,5 +16,44 @@ class PlacarActivity : AppCompatActivity() {
         setContentView(R.layout.activity_placar)
 
         placarViewlModel = ViewModelProviders.of(this).get(PlacarViewModel::class.java)
+
+        placarViewlModel?.placarVisitante?.observe(this, Observer {
+                tvPontoB.text = it?.toString()
+        })
+
+        placarViewlModel?.placarCasa?.observe(this, Observer {
+            tvPontoA.text = it?.toString()
+        })
+
+
+        btA1.setOnClickListener {
+            placarViewlModel?.pontoCasa(1)
+        }
+
+        btA2.setOnClickListener {
+            placarViewlModel?.pontoCasa(2)
+        }
+
+        btA3.setOnClickListener {
+            placarViewlModel?.pontoCasa(3)
+        }
+
+        btB1.setOnClickListener {
+            placarViewlModel?.pontoVisitante(1)
+        }
+
+
+        btB2.setOnClickListener {
+            placarViewlModel?.pontoVisitante(2)
+        }
+
+
+        btB3.setOnClickListener {
+            placarViewlModel?.pontoVisitante(3)
+        }
+
+     btReiniciar.setOnClickListener {
+         placarViewlModel?.reiniciar()
+     }
     }
 }
--- app/src/main/java/br/com/rafaelsergio/ciclodevida/viewmodelcomlivedata/PlacarViewModel.kt
+++ app/src/main/java/br/com/rafaelsergio/ciclodevida/viewmodelcomlivedata/PlacarViewModel.kt
@@ -1,10 +1,32 @@
 package br.com.rafaelsergio.ciclodevida.viewmodelcomlivedata
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class PlacarViewModel: ViewModel(){
 
+    //liveData
 
+    val placarCasa = MutableLiveData<Int>()
+    val placarVisitante = MutableLiveData<Int>()
 
+    init {
+        placarCasa.value = 0
+        placarVisitante.value = 0
+    }
+
+    fun pontoCasa(ponto: Int){
+        placarCasa.value = placarCasa.value?.plus(ponto)
+    }
+
+    fun pontoVisitante(ponto: Int){
+        placarVisitante.value = placarVisitante.value?.plus(ponto)
+    }
+
+    fun reiniciar(){
+
+        placarCasa.value = 0
+        placarVisitante.value = 0
+    }
 
 }
