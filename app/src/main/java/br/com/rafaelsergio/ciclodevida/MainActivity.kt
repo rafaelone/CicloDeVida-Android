3abcaee08a67063e1d79999ce0983ab24e4f598f
Rafael <logonrmlocal@fiap.com.br>
2018-09-01 16:07:51 -0300

    fazendo o calculo dos valores

--- app/src/main/java/br/com/rafaelsergio/ciclodevida/MainActivity.kt
+++ app/src/main/java/br/com/rafaelsergio/ciclodevida/MainActivity.kt
@@ -1,12 +1,42 @@
 package br.com.rafaelsergio.ciclodevida
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        atualizarPlacarCasa()
+        atualizarPlacarVisitante()
+
+        btCasa.setOnClickListener{
+            mainViewModel.placarCasa = mainViewModel.placarCasa + 1
+            atualizarPlacarCasa()
+        }
+
+        btVisitante.setOnClickListener {
+            mainViewModel.placarVisitante = mainViewModel.placarVisitante + 1
+            atualizarPlacarVisitante()
+        }
+
     }
+
+    private fun atualizarPlacarCasa(){
+        tvCasa.text = mainViewModel.placarCasa.toString()
+    }
+    private fun atualizarPlacarVisitante(){
+        tvVisitante.text = mainViewModel.placarVisitante.toString()
+    }
+
 }
---
+++ app/src/main/java/br/com/rafaelsergio/ciclodevida/MainViewModel.kt
@@ -0,0 +1,10 @@
+package br.com.rafaelsergio.ciclodevida
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var placarCasa = 0
+    var placarVisitante = 0
+
+}
